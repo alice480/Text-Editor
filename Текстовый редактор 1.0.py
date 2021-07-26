2396d6877977630dbef469c95264e086e2e20757
alice480 <65951454+alice480@users.noreply.github.com>
2021-07-26 23:17:26 +0900

    Add files via upload

---
+++ Текстовый редактор 1.0.py
@@ -0,0 +1,84 @@
+import sys
+
+from PyQt5.QtWidgets import QApplication, QMainWindow
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(447, 408)
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.lineEdit = QtWidgets.QLineEdit(self.centralwidget)
+        self.lineEdit.setGeometry(QtCore.QRect(20, 30, 113, 20))
+        self.lineEdit.setObjectName("lineEdit")
+        self.pushButton = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton.setGeometry(QtCore.QRect(20, 60, 111, 23))
+        self.pushButton.setObjectName("pushButton")
+        self.pushButton_2 = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton_2.setGeometry(QtCore.QRect(20, 90, 111, 23))
+        self.pushButton_2.setObjectName("pushButton_2")
+        self.pushButton_3 = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton_3.setGeometry(QtCore.QRect(20, 120, 111, 23))
+        self.pushButton_3.setObjectName("pushButton_3")
+        self.textEdit = QtWidgets.QTextEdit(self.centralwidget)
+        self.textEdit.setGeometry(QtCore.QRect(150, 30, 271, 291))
+        self.textEdit.setObjectName("textEdit")
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.menubar = QtWidgets.QMenuBar(MainWindow)
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 447, 21))
+        self.menubar.setObjectName("menubar")
+        MainWindow.setMenuBar(self.menubar)
+        self.statusbar = QtWidgets.QStatusBar(MainWindow)
+        self.statusbar.setObjectName("statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "Текстовый редактор 1.0"))
+        self.pushButton.setText(_translate("MainWindow", "Создать новый"))
+        self.pushButton_2.setText(_translate("MainWindow", "Сохранить файл"))
+        self.pushButton_3.setText(_translate("MainWindow", "Открыть файл"))
+
+
+class MyWidget(QMainWindow, Ui_MainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setupUi(self)
+        self.pushButton.clicked.connect(self.new)
+        self.pushButton_2.clicked.connect(self.save)
+        self.pushButton_3.clicked.connect(self.open_file)
+
+    def new(self):
+        self.textEdit.clear()
+
+    def save(self):
+        text = self.textEdit.toPlainText()
+        name = self.lineEdit.text()
+        f = open(name, mode='w', encoding='utf-8')
+        f.write(text)
+
+    def open_file(self):
+        self.textEdit.clear()
+        name = self.lineEdit.text()
+        try:
+            f = open(name, mode='r', encoding='utf-8')
+            lines = f.readlines()
+            for elem in lines:
+                self.textEdit.append(elem.strip('\n'))
+        except FileNotFoundError:
+            self.statusBar().showMessage(f'Файл "{name}" не существует')
+
+def except_hook(cls, exception, traceback):
+    sys.__excepthook__(cls, exception, traceback)
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = MyWidget()
+    ex.show()
+    sys.excepthook = except_hook
+    sys.exit(app.exec())
